f17202058fc61fb62c4b6c8101e642d6699848e1
Sophia Ho <swho@andrew.cmu.edu>
2021-10-16 02:01:51 -0400

    changed styling of product select details

--- script.js
+++ script.js
@@ -51,3 +51,37 @@ function addToCart() {
   addItemToCart()
   displayCartNotification()
 }
+
+function hoverSize(size){
+  let selSize = document.getElementById("selected-size")
+  selSize.innerHTML = size
+}
+
+function hoverColor(color) {
+  let selColor = document.getElementById("selected-color")
+  selColor.innerHTML = color
+}
+
+function mouseOutSize() {
+  let sizeSel = document.getElementsByName("size")
+  var size = ""
+  for (let i=0; i<4; i++) {
+    if (sizeSel[i].checked) {
+      size = sizeSel[i].value
+    }
+  }
+  size = size.charAt(0).toUpperCase() + size.slice(1)
+  document.getElementById("selected-size").innerHTML = size
+}
+
+function mouseOutColor() {
+  let colorSel = document.getElementsByName("color")
+  var color = ""
+  for (let i=0; i<4; i++) {
+    if (colorSel[i].checked) {
+      color = colorSel[i].value
+    }
+  }
+  color = color.charAt(0).toUpperCase() + color.slice(1)
+  document.getElementById("selected-color").innerHTML = color
+}
